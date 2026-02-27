693aa1c3e28251e29cf1f16885678874b21d17b1
PhamLeThanhNhiet <nhiethiz@gmail.com>
2026-02-27 12:17:02 +0700

    feat: add module sggt_combo_component

---
+++ modules/sggt_combo_component/Ext/Language/vi_VN.lang.ext.php
@@ -0,0 +1,12 @@
+<?php 
+ //WARNING: The contents of this file are auto-generated
+
+
+//THIS FILE IS AUTO GENERATED, DO NOT MODIFY
+$mod_strings['LBL_SGGT_COMBO_COMPONENT_AOS_PRODUCTS_FROM_AOS_PRODUCTS_TITLE'] = 'Products';
+
+
+// created: 2026-02-27 03:46:22
+$mod_strings['LBL_SGGT_COMBO_COMPONENT_AOS_PRODUCTS_FROM_AOS_PRODUCTS_TITLE'] = 'Sản phẩm cha';
+
+?>
---
+++ modules/sggt_combo_component/metadata/subpanels/AOS_Products_subpanel_sggt_combo_component_aos_products.php
@@ -0,0 +1,33 @@
+<?php
+// created: 2026-02-27 05:56:45
+$subpanel_layout['list_fields'] = array (
+  'name' => 
+  array (
+    'vname' => 'LBL_NAME',
+    'widget_class' => 'SubPanelDetailViewLink',
+    'width' => '45%',
+    'default' => true,
+  ),
+  'date_modified' => 
+  array (
+    'vname' => 'LBL_DATE_MODIFIED',
+    'width' => '45%',
+    'default' => true,
+  ),
+  'edit_button' => 
+  array (
+    'vname' => 'LBL_EDIT_BUTTON',
+    'widget_class' => 'SubPanelEditButton',
+    'module' => 'sggt_combo_component',
+    'width' => '4%',
+    'default' => true,
+  ),
+  'remove_button' => 
+  array (
+    'vname' => 'LBL_REMOVE',
+    'widget_class' => 'SubPanelRemoveButton',
+    'module' => 'sggt_combo_component',
+    'width' => '5%',
+    'default' => true,
+  ),
+);
